95e7a14be245e6f1bc29f6ca796c1b316acf71fa
mahinfah <mahinfah1234@gmail.com>
2025-06-08 22:49:16 +0600

    ok

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -98,7 +98,7 @@
             this.label1.AutoSize = true;
             this.label1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.label1.Font = new System.Drawing.Font("Trebuchet MS", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(3, 111);
+            this.label1.Location = new System.Drawing.Point(-5, 111);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(68, 27);
             this.label1.TabIndex = 0;
